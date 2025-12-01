c95cdeb632b86595ea53f12b8eef0f765670598a
Jordane Rubal <jordane.rubal@afpa-dev-pompey.fr>
2025-12-01 16:59:10 +0100

    app js en cours, html liste ok(?)

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,79 @@ var films = [
     }
 ];
 // TODO
+//récupérer le tableau pour faire l'affichage de [films]
+let table = document.querySelector("#table")
+
+function movie() { //créer une fonction pour réutiliser l'affichage pour ajout utilisateur
+    films.forEach(film => {
+        table.insertAdjacentHTML("beforeend", `<tr><td>${film.title}</td><td>${film.years}</td><td>${film.authors}</td></tr>`)
+    })
+}
+
+movie();
+//récupérer le btn aj pour l'eventListener
+let aj = document.querySelector("#btn_ajouter")
+//au click, ajouter form dans la section, récupérer la value des inputs et compléter le tableau
+aj.addEventListener("click", function () {
+    let secAj = document.querySelector("#section_ajouter");
+    secAj.innerHTML = `<form id="form_ajouter">
+                <input type="text" name="titre_aj" id="titre_aj" placeholder="Titre">
+                <input type="number" name="annee_aj" id="annee_aj" placeholder="Année de sortie">
+                <input type="text" name="real_aj" id="real_aj" placeholder="Réalisateur">
+                <button type="submit" id="save">Ajouter</button></form>`;
+
+    document.querySelector("#save").addEventListener("click", function (event) {
+        event.preventDefault();
+        //vérifier les entrées des inputs, ok=afficher, !ok=message erreur
+        let ok = true; //bouton on/off pour décider de l'affichage du contenu utilisateur
+        let erreur = document.querySelector("#erreur")
+        let txterreur = ""; //conserver et MàJ les erreurs pour affichage message final
+
+        //vérification input Titre
+        let titre = document.querySelector("#titre_aj").value;
+        console.log(titre);
+        if (titre.length < 2) {
+            ok = false;
+            txterreur += `<li>Titre trop court, 2 caractères minimum.</li>`
+        }
+
+        //vérification input Année
+        let currentYear = new Date().getFullYear();
+        let annee = document.querySelector("#annee_aj").value;
+        let REannee = new RegExp("^\\d{4}$"); //commence et termine par un chiffre. doit en avoir 4
+        if (!REannee.test(annee)) {
+            ok = false;
+            txterreur += `<li>L'année doit être composée de 4 chiffres. `
+        }
+        if (parseInt(annee) < 1900 || parseInt(annee) > currentYear) {
+            ok = false;
+            txterreur += `<li>L'année doit être compris entre 1900 et aujourd'hui.</li>`
+        }
+
+        //vérification input Réalisateur
+        let real = document.querySelector("#real_aj").value;
+        let REreal = new RegExp("[a-zA-Z]{5,}"); //5 lettres ou plus
+        if (!REreal.test(real)) {
+            ok = false;
+            txterreur += `<li>Le nom du réalisateur doit être composé de 5 lettres minimum.</li>`
+        }
+
+        //affichage dans la section erreur en fonction de ok
+        console.log(ok)
+        if (ok) {
+            films.push({title: titre, years: parseInt(annee), authors: real})
+            let lastFilm = films[films.length - 1];
+            table.insertAdjacentHTML("beforeend", `<tr><td>${lastFilm.title}</td><td>${lastFilm.years}</td><td>${lastFilm.authors}</td></tr>`)
+            erreur.innerHTML = `<p>Film ajouté avec succès.</p>`
+            setTimeout(() => {
+                erreur.setAttribute("style", "display: none");
+            }, 3000);
+
+        } else {
+            erreur.innerHTML = `<p>Attention! Erreur(s) dans le formulaire : <ul>${txterreur}</ul></p>`
+            setTimeout(() => {
+                erreur.setAttribute("style", "display: none");
+            }, 5000);
+        }
+    })
+})
